4816a015162ff4b0469c81ff837f941e714dd28c
holyfs <egcg1986@gmail.com>
2022-05-17 08:08:09 +0000

    Entrega ejercicio de conditional card profile

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import "../style/index.css";
 
-/**
+/* *
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
  * 
@@ -20,8 +20,8 @@ import "../style/index.css";
         role: null,
         country: null,
         city: null
-    }
- */
+    } */
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
@@ -33,14 +33,24 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+           <h1> ${!variables.name ? "Tu Nombre" : variables.name} ${
+    !variables.lastname ? "Tu Apellido" : variables.lastname
+  }</h1> 
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}${variables.country}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              !variables.twitter ? "Twitter" : variables.twitter
+            }" ><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              !variables.linkedin ? "Linkedin" : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
